b4b6aa03479b9cb18bac2327697c0894c99cf798
richardsaid <richard@experiencemeda.com>
2016-02-15 09:58:59 +0000

    Source control commit added

---
+++ workflow/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  workflow
+//
+//  Created by Richard on 15/02/2016.
+//  Copyright © 2016 Richard. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
